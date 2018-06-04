b41bb97670e70fea91bf3adcee071c290babea9b
Adrien Bezannier <adrien.bezannier@gmail.com>
2018-06-05 01:38:45 +0200

    Surprise

--- inc/lem_in.h
+++ inc/lem_in.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lem_in.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abezanni <abezanni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adibou <adibou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/25 16:06:51 by abezanni          #+#    #+#             */
-/*   Updated: 2018/06/04 17:42:44 by abezanni         ###   ########.fr       */
+/*   Updated: 2018/06/04 22:23:28 by adibou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,6 @@ typedef struct		s_data
 	int				*start;
 	int				*end;
 	t_room			**rooms;
-	int				nb_ways;
-	int				*by_way;
 	int				option;
 }					t_data;
 
