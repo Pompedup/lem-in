9041d55e29f6eca7ae099436cfaef82f1cca800b
Adrien Bezannier <adrien.bezannier@gmail.com>
2018-06-06 18:17:30 +0200

    lem_in.c

--- src/lem_in.c
+++ src/lem_in.c
@@ -3,14 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   lem_in.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccoupez <ccoupez@student.42.fr>            +#+  +:+       +#+        */
+/*   By: abezanni <abezanni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/25 15:13:14 by abezanni          #+#    #+#             */
-/*   Updated: 2018/06/06 18:10:09 by ccoupez          ###   ########.fr       */
+/*   Updated: 2018/06/06 18:17:10 by abezanni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+void	ft_exit(char c)
+{
+	char print[2];
+	char *tofree;
+
+	if (c)
+	{
+		print[0] = c;
+		print[1] = '\0';
+		ft_putendl((tofree = ft_strjoin("lem-in: illegal option -- ", print)));
+		free(tofree);
+	}
+	ft_putendl("usage: ./lem-in [-e] < file");
+	exit(0);
+}
+
+void	ft_add_option(char c, t_data *data)
+{
+	int add;
+
+	add = 0;
+	if (c == 'e')
+		add = 1;
+	if (!(data->option & add))
+		data->option += add;
+}
+
+void	ft_check_option(t_data *data, int ac, char **av)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < ac)
+	{
+		if (*av[i] != '-')
+			ft_exit('\0');
+		j = 1;
+		while (av[i][j])
+		{
+			if (!ft_strchr("e", av[i][j]))
+				ft_exit(av[i][j]);
+			else
+				ft_add_option(av[i][j], data);
+			j++;
+		}
+		i++;
+	}
+}
 
 void	ft_init_struct(t_data *data)
 {
@@ -20,6 +70,7 @@ void	ft_init_struct(t_data *data)
 	data->start = NULL;
 	data->end = NULL;
 	data->rooms = NULL;
+	data->option = 0;
 }
 
 /*
@@ -40,13 +91,9 @@ int		main(int ac, char **av)
 {
 	t_data data;
 
-	data.option = 0;
-	if (ac > 1)
-	{
-		(void)av;
-		data.option = 1;
-	}
 	ft_init_struct(&data);
+	if (ac > 1)
+		ft_check_option(&data, ac, av);
 	if (!ft_parse(&data))
 		return (ft_invalide_file());
 	ft_resolve(&data);
