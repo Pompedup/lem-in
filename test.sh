2f6a935cbe58fd375d676ddbb1f42c34c62b2879
Adrien Bezannier <adrien.bezannier@gmail.com>
2018-06-06 16:46:56 +0200

    test

---
+++ test.sh
@@ -0,0 +1,18 @@
+
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="ccoupez@e3r12p16.42.f"
+CORRECT_NAME="coupez"
+CORRECT_EMAIL="ccoupez@student.42.fr"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
